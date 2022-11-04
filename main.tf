996771d810ee625b02bd51adbad081f05a59698c
melnottp <92871211+melnottp@users.noreply.github.com>
2022-11-04 18:21:52 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -100,6 +100,17 @@ resource "flexibleengine_networking_secgroup_rule_v2" "secgroup_rule_ingress4" {
   security_group_id = flexibleengine_networking_secgroup_v2.secgroup.id
 }
 
+# Add rules to access MySQL
+resource "flexibleengine_networking_secgroup_rule_v2" "secgroup_rule_ingress4" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = "${var.mysql_port}"
+  port_range_max    = "${var.mysql_port}"
+  remote_ip_prefix  = "${var.subnet_cidr}"
+  security_group_id = flexibleengine_networking_secgroup_v2.secgroup.id
+}
+
 # security group rule to access Bastion
 resource "flexibleengine_networking_secgroup_rule_v2" "guacamole_rule_ingress4" {
   direction         = "ingress"
