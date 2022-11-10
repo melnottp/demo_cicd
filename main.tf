6dbfe4ed6deaf7952801a2c6adbc848b279a1516
melnottp <92871211+melnottp@users.noreply.github.com>
2022-11-10 14:20:07 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -50,25 +50,46 @@ resource "flexibleengine_vpc_v1" "vpc" {
   cidr = "${var.vpc_cidr}"
 }
 
-# Create Router interface inside the VPC
-resource "flexibleengine_networking_router_interface_v2" "router_interface" {
-  router_id = flexibleengine_vpc_v1.vpc.id
-  subnet_id = flexibleengine_networking_subnet_v2.subnet.id
-}
-
-# Create network inside the VPC
-resource "flexibleengine_networking_network_v2" "net" {
-  name           = "${var.project}-network-${random_string.id.result}"
+# Create Frontend network inside the VPC
+resource "flexibleengine_networking_network_v2" "front_net" {
+  name           = "${var.project}-front_net-${random_string.id.result}"
   admin_state_up = "true"
 }
 
-# Create subnet inside the network
-resource "flexibleengine_networking_subnet_v2" "subnet" {
-  name            = "${var.project}-subnet-${random_string.id.result}"
-  cidr            = "${var.subnet_cidr}"
-  network_id      = flexibleengine_networking_network_v2.net.id
-  gateway_ip      = "${var.gateway_ip}"
+# Create Backend network inside the VPC
+resource "flexibleengine_networking_network_v2" "back_net" {
+  name           = "${var.project}-back_net-${random_string.id.result}"
+  admin_state_up = "true"
+}
+
+# Create Frontend subnet inside the network
+resource "flexibleengine_networking_subnet_v2" "front_subnet" {
+  name            = "${var.project}-front_subnet-${random_string.id.result}"
+  cidr            = "${var.front_subnet_cidr}"
+  network_id      = flexibleengine_networking_network_v2.front_net.id
+  gateway_ip      = "${var.front_gateway_ip}"
   dns_nameservers = ["100.125.0.41", "100.126.0.41"]
+}
+
+# Create Backend subnet inside the network
+resource "flexibleengine_networking_subnet_v2" "back_subnet" {
+  name            = "${var.project}-back_subnet-${random_string.id.result}"
+  cidr            = "${var.back_subnet_cidr}"
+  network_id      = flexibleengine_networking_network_v2.back_net.id
+  gateway_ip      = "${var.back_gateway_ip}"
+  dns_nameservers = ["100.125.0.41", "100.126.0.41"]
+}
+
+# Create Router interface for Frontend Network
+resource "flexibleengine_networking_router_interface_v2" "front_router_interface" {
+  router_id = flexibleengine_vpc_v1.vpc.id
+  subnet_id = flexibleengine_networking_subnet_v2.front_subnet.id
+}
+
+# Create Router interface for Backend Network
+resource "flexibleengine_networking_router_interface_v2" "back_router_interface" {
+  router_id = flexibleengine_vpc_v1.vpc.id
+  subnet_id = flexibleengine_networking_subnet_v2.back_subnet.id
 }
 
 #Create an Elastic IP for Bastion VM
@@ -155,19 +176,27 @@ resource "flexibleengine_nat_gateway_v2" "nat_1" {
   subnet_id   = flexibleengine_networking_network_v2.net.id
 }
 
-#Add SNAT rule
+#Add SNAT rule for Frontend subnet
 resource "flexibleengine_nat_snat_rule_v2" "snat_1" {
   depends_on = [time_sleep.wait_for_vpc]  
   nat_gateway_id = flexibleengine_nat_gateway_v2.nat_1.id
   floating_ip_id = flexibleengine_vpc_eip_v1.eip_natgw.id
-  subnet_id      = flexibleengine_networking_network_v2.net.id
+  subnet_id      = flexibleengine_networking_network_v2.front_net.id
+}
+
+#Add SNAT rule for Backend subnet
+resource "flexibleengine_nat_snat_rule_v2" "snat_2" {
+  depends_on = [time_sleep.wait_for_vpc]  
+  nat_gateway_id = flexibleengine_nat_gateway_v2.nat_1.id
+  floating_ip_id = flexibleengine_vpc_eip_v1.eip_natgw.id
+  subnet_id      = flexibleengine_networking_network_v2.back_net.id
 }
 
 #Create ELB
 resource "flexibleengine_lb_loadbalancer_v2" "elb_1" {
   depends_on = [time_sleep.wait_for_vpc]  
   description   = "ELB for project ${var.project} (${random_string.id.result})"
-  vip_subnet_id = flexibleengine_networking_subnet_v2.subnet.id
+  vip_subnet_id = flexibleengine_networking_subnet_v2.back_subnet.id
   name = "${var.project}-ELB-${random_string.id.result}"
 
 }
@@ -206,7 +235,7 @@ resource "flexibleengine_compute_instance_v2" "instance" {
   user_data = data.template_cloudinit_config.config.rendered
   availability_zone = "eu-west-0a"
   network {
-    uuid = flexibleengine_networking_network_v2.net.id
+    uuid = flexibleengine_networking_network_v2.front_net.id
   }
   block_device { # Boots from volume
     uuid                  = "c2280a5f-159f-4489-a107-7cf0c7efdb21"
@@ -226,13 +255,13 @@ resource "flexibleengine_compute_floatingip_associate_v2" "fip_1" {
 
 #Create MySQL RDS
 resource "flexibleengine_rds_instance_v3" "instance" {
-  depends_on = [flexibleengine_networking_subnet_v2.subnet]
+  depends_on = [flexibleengine_networking_subnet_v2.back_net]
   name              = "${var.project}-MySQL-${random_string.id.result}"
   flavor            = "rds.mysql.c6.large.2"
   availability_zone = ["eu-west-0b"]
   security_group_id = flexibleengine_networking_secgroup_v2.secgroup.id
   vpc_id            = flexibleengine_vpc_v1.vpc.id
-  subnet_id         = flexibleengine_networking_network_v2.net.id
+  subnet_id         = flexibleengine_networking_network_v2.back_net.id
 
   db {
     type     = "MySQL"
@@ -276,7 +305,7 @@ resource "flexibleengine_cce_cluster_v3" "cluster" {
   cluster_type           = "VirtualMachine"
   flavor_id              = "cce.s1.small"
   vpc_id                 = flexibleengine_vpc_v1.vpc.id
-  subnet_id              = flexibleengine_networking_network_v2.net.id
+  subnet_id              = flexibleengine_networking_network_v2.back_net.id
   container_network_type = "overlay_l2"
   authentication_mode    = "rbac"
 }
